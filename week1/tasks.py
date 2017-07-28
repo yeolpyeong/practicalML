2079e177c80ddbdf57bd6507f120d38d4fa0502c
1040151 <yeolpyeong@gmail.com>
2017-07-28 19:21:07 +0900

    bitcoinBot

---
+++ week1/tasks.py
@@ -0,0 +1,46 @@
+from microsoftbotframework import ReplyToActivity
+import requests
+import json
+import datetime
+import random
+
+greetings = [ "hi", "hello", "hey", "yo", "greetings" ]
+greetings_responses = [ "Hi there." , "Greetings man.", "Hello there.", "Hey." ]
+
+def response(message):
+  if message["type"] == "message":
+    ReplyToActivity(fill=message, text=reply(message)).send()  
+        
+def reply(message):
+    url = 'https://api.korbit.co.kr/v1/ticker/detailed'
+    params = {'format': json}
+
+    response = requests.get(url, params=params)
+    data = response.json()
+    '''
+    if 'predict' in message["text"]:
+      diff_24h = data['diff_24h']
+      diff_per_24h = data['diff_per_24h']
+      bid = data['bid']
+      ask = data['ask']
+      low = data['low']
+      high = data['high']
+      volume = data['volume']
+      res = 4444
+      '''
+    if 'bitcoin' in message["text"]:
+      price = data['last']
+      utcnow = datetime.datetime.utcnow()
+      time_gap = datetime.timedelta(hours=9)
+      current = str(utcnow + time_gap)
+      res = current + " price: " + price
+      #print(res)
+      
+    elif message["text"] in greetings:
+      res = random.choice(greetings_responses) + " I am low-level bitcoinBot.\nI can ONLY reply current bitcoin price.\nPlease ask including 'bitcoin'."
+      
+    else: 
+      res = "Sorry, I am low-level bitcoinBot.\n Please ask including 'bitcoin'."
+      
+    return res
+  
