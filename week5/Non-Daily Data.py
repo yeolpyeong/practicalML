1b98d70070a932091cb274c07ed8db98a1ad84e4
KimPyeongyeol(김평열) <yeolpyeong@gmail.com>
2017-07-30 20:22:17 +0900

    Update Non-Daily Data.py

--- week5/Non-Daily Data.py
+++ week5/Non-Daily Data.py
@@ -2,7 +2,7 @@ import pandas as pd
 from fbprophet import Prophet
 import matplotlib.pyplot as plt
 
-df = pd.read_csv('/home/yeolpyeong/pragmaticML/week5/example_retail_sales.csv')
+df = pd.read_csv('./example_retail_sales.csv')
 m = Prophet().fit(df)
 future = m.make_future_dataframe(periods=3652)
 fcst = m.predict(future)
